26a13381e064e27c100b6cffbba6bf3aaca3981e
Manoel Carvalho <manoelmecat@gmail.com>
2024-05-15 09:58:48 -0300

    feat: add lognIn screen

---
+++ app/index.js
@@ -0,0 +1,8 @@
+import { ActivityIndicator, View } from "react-native";
+export default function Home() {
+  return (
+    <View className="flex-1 justify-center">
+      <ActivityIndicator size="large" color="#7F83ED" />
+    </View>
+  );
+}
---
+++ app/signIn.js
@@ -0,0 +1,8 @@
+import { Text, View } from "react-native";
+export default function SignIn() {
+  return (
+    <View className="flex-1 justify-center">
+      <Text>SignIn</Text>
+    </View>
+  );
+}
